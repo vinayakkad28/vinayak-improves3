c6d3c3ba9369d8b1aa3bc3b5118c14885993fd64
Vinayak Kad <vinayakkad28@gmail.com>
2026-01-18 00:27:41 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Vercel Deployment Successful 🚀</h1>
+      <p>Your project is now production ready.</p>
+    </div>
+  )
+}
